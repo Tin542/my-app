35234378de4b729a5a307d6298328d65e89878db
TinNT <nguyenthanhtin@pnt.edu.vn>
2023-11-28 11:10:28 +0700

    add auth folder

---
+++ auth/auth.controller.js
@@ -0,0 +1,112 @@
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt"); // encrypt password
+
+const resJSON = require("../constants/responseJSON");
+
+const prisma = new PrismaClient();
+
+function authController() {
+  const SELF = {
+    enCodePass: (password) => {
+      return bcrypt
+        .hash(password, 10) // change encode password - 10 : salt
+        .then((hash) => {
+          return Promise.resolve(hash);
+        })
+        .catch((err) => {
+          Logger.error(`encrypt password fail: ${err}`);
+        });
+    },
+  };
+  return {
+    login: async (req, res) => {
+      try {
+        let data = req.body; // get data from body;
+        // check valid data
+        if (!data.username || !data.password) {
+          return res
+            .status(400)
+            .json(resJSON(false, 400, "Please enter required fields", null));
+        }
+        // check if user existed
+        const user = await prisma.user.findUnique({
+          where: { username: data.username },
+        });
+        if (!user) {
+          return res
+            .status(404)
+            .json(resJSON(false, 404, "Account not found", null));
+        }
+        // check login
+        const result = bcrypt.compare(data?.password.trim(), user.password);
+        if (!result) {
+          return res
+            .status(404)
+            .json(resJSON(false, 404, "Wrong password", null));
+        } else {
+          return res
+            .status(200)
+            .json(resJSON(true, 200, "Login success", user));
+        }
+      } catch (error) {
+        res.status(500).json(resJSON(false, 500, "Something went wrong", null));
+      } finally {
+        async () => await prisma.$disconnect();
+      }
+    },
+    register: async (req, res) => {
+      try {
+        let data = req.body;
+        // check data validation
+        if (!data.username || !data.password || !data.full_name) {
+          return res
+            .status(400)
+            .json(resJSON(false, 400, "Please enter reuired fields", null));
+        }
+        // check duplicate data
+        const user = await prisma.user.findUnique({
+          where: { username: data.username },
+        });
+        if (user) {
+          return res
+            .status(400)
+            .json(resJSON(false, 400, "Username already in use", null));
+        }
+        SELF.enCodePass(data.password).then(async (hash) => {
+          const result = await prisma.user.create({
+            data: {
+              username: data.username,
+              password: hash,
+              full_name: data.full_name,
+              score: 0,
+              create_date: new Date(),
+              update_date: new Date(),
+              role: {
+                create: [
+                  {
+                    create_date: new Date(),
+                    update_date: new Date(),
+                    role: {
+                      connect: {
+                        role_id: 1,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          });
+          return res
+            .status(200)
+            .json(resJSON(true, 200, "Register successfull", result));
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(500).json(resJSON(false, 500, "Something went wrong", null));
+      } finally {
+        async () => await prisma.$disconnect();
+      }
+    },
+  };
+}
+module.exports = new authController();
--- users/users.routes.js
+++ users/users.routes.js
@@ -4,8 +4,6 @@ const router = express.Router({});
 const userController = require('./users.controller');
 
 router.get('/all', userController.getAllUser);
-router.post('/register', userController.register);
-router.post('/login', userController.login);
 router.put('/update/:uid', userController.updateUser);
 router.delete('/delete/:uid', userController.deleteUser);
 router.put('/update-score/:uid', userController.updateScore);
