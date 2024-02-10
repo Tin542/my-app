322b64a812beb70b2d130f2c38908c041b363320
bin <tinnt0504@gmail.com>
2024-02-10 13:08:31 +0700

    f

--- feature/product/product.routes.js
+++ feature/product/product.routes.js
@@ -4,7 +4,7 @@ const router = express.Router({});
 const productController = require("./product.controller");
 const authenMiddleware = require("../auth/auth.middleware");
 
-router.get("/get-all", authenMiddleware.isCustomer, productController.getAllProduct);
+router.get("/get-all", productController.getAllProduct);
 router.get("/get-detail/:id", productController.getProductDetail);
 router.post("/create", productController.addProduct);
 router.post("/edit", productController.editProduct);
